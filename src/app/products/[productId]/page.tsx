4cf09252f640284818e5fcee26fb1a1ef15d2efc
Mohammad Abuzar <mohammad.abuzar@unthinkable.co>
2024-10-23 00:16:55 +0530

    Dynamic Routes

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,9 @@
+export default async function ProductsDetails({
+  params,
+}: {
+  params: { productId: string };
+}) {
+  const { productId } = await params;
+
+  return <h1>Product Details Page {productId}</h1>;
+}
