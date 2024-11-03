ff76488c9dd9f2dde3344c2212a8caa454efabd1
Mohammad Abuzar <mohammad.abuzar@unthinkable.co>
2024-11-03 19:27:33 +0530

    Added Meta Data And Dynamic Meta Data

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+    title: 'About Us',
+    
+}
+
 export default function About(){
     return <h1>About Us</h1>
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,9 +1,23 @@
-export default async function ProductsDetails({
-  params,
-}: {
-  params: { productId: string };
-}) {
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  // Awaiting an example async operation for `title`
+  const title = await Promise.resolve(`Product ${params.productId}`);
+
+  return {
+    title,
+  };
+};
+
+export default async function ProductDetails({ params }: Props) {
+  // Ensure `params` is awaited
   const { productId } = await params;
 
-  return <h1>Product Details Page {productId}</h1>
+  return <h1>Product Details Page {productId}</h1>;
 }
